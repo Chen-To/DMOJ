8fa054d8aa6e0f730dec70a769f6111aafc9fbff
Chen-To <32133168+Chen-To@users.noreply.github.com>
2020-09-09 11:43:10 -0400

    Create Hailstone Numbers.java

---
+++ Uncategorized/Hailstone Numbers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner (System.in);
+    int number = in.nextInt();
+    int counter = 0;
+    while (number != 1){
+      if (number % 2 == 0){
+        number = number/2;
+      }  
+      else{
+        number = number*3+1;
+      }
+      counter ++;
+    }
+    System.out.print(counter);
+  }
+}

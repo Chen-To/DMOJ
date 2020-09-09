a4886954cb535e6c0aa514cf43537ad9974a6afa
Chen-To <32133168+Chen-To@users.noreply.github.com>
2020-09-08 20:44:55 -0400

    Update Secret Santa.java

--- GFSSOC/Secret Santa.java
+++ GFSSOC/Secret Santa.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 public class SecretSanta{
-	static BufferedReader br;
+    static BufferedReader br;
     static PrintWriter out;
     static StringTokenizer st;
     static Map<Integer,Integer> m = new HashMap<Integer,Integer>();
